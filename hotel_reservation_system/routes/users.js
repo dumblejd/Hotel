069bee2f405f67d098f31b52c9730497c4985382
carnivoreYY <yingyi814@gmail.com>
2018-11-19 16:19:53 -0600

    add admin user

--- hotel_reservation_system/routes/users.js
+++ hotel_reservation_system/routes/users.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var monk = require('monk');
+var db = monk('localhost:27017/hotel');
+
+router.get('/', function(req, res) {
+    var collection = db.get('users');
+    collection.find({}, function(err, videos){
+        if (err) throw err;
+        res.json(videos);
+    });
 });
 
 module.exports = router;
